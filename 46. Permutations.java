92738af9adcbf35912ace89a4ea5bb64627bf46f
ShwTmpLC <69881269+ShwTmpLC@users.noreply.github.com>
2020-08-30 02:14:36 -0700

    Create 46. Permutations.java

---
+++ 46. Permutations.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> curResult = new ArrayList<>();
+        
+        boolean[] used = new boolean[nums.length];
+        
+        backtrack(nums, used, curResult, result);
+        return result;
+    }
+
+    private void backtrack(int[] nums, boolean[] used, List<Integer> curResult, List<List<Integer>> result) {
+        if (curResult.size() == nums.length){
+            result.add(new ArrayList<>(curResult));
+            return;
+        }
+        
+        for (int i = 0; i < nums.length; i++){ 
+            if (used[i]) continue;
+            curResult.add(nums[i]);
+            used[i] = true;
+            backtrack(nums, used, curResult, result);
+            curResult.remove(curResult.size() - 1);
+            used[i] = false;
+        }
+    } 
+}
